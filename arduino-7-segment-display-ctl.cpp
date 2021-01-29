20605640fc0c28a2c2b7b1c1bc8f2ed7870376e0
Dominik Rzecki <dominik.rzecki@gmail.com>
2021-01-29 14:54:16 +0100

    files added

---
+++ arduino-7-segment-display-ctl.cpp
@@ -0,0 +1,85 @@
+/* arduino-7-segment-display-ctl.cpp
+ *
+ * Copyright 2021 Dominik Rzecki
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+ 
+
+#include "arduino-7-segment-display-ctl.hpp"
+
+_7SegmentDisplay::_7SegmentDisplay(byte a, byte b, byte c, byte d, byte e, byte f, byte g, bool lowDriven = false){
+	sd7_pins[0] = a;
+	sd7_pins[1] = b;
+	sd7_pins[2] = c;
+	sd7_pins[3] = d;
+	sd7_pins[4] = e;
+	sd7_pins[5] = f;
+	sd7_pins[6] = g;
+	sd7_lowDriven = lowDriven;
+	setPins();
+}
+
+_7SegmentDisplay::_7SegmentDisplay(byte pins[7]){
+	for(int i = 0; i<7; i++){
+		sd7_pins[i] = pins[i];
+	}
+	setPins();
+}
+
+void _7SegmentDisplay::TurnOff(){
+	if(sd7_lowDriven){
+		for(int i = 0; i<7; i++){
+			digitalWrite(sd7_pins[i], HIGH);
+		}
+	} else {
+		for(int i = 0; i<7; i++){
+			digitalWrite(sd7_pins[i], LOW);
+		}
+	}
+}
+
+void _7SegmentDisplay::Display(int num){
+	if(sd7_lowDriven){
+		for(int i = 0; i<7; i++){
+			digitalWrite(sd7_pins[i], !sd7_num[num][i]);
+		}
+	} else {
+		for(int i = 0; i<7; i++){
+			digitalWrite(sd7_pins[i], sd7_num[num][i]);
+		}
+	}
+}
+
+void _7SegmentDisplay::setPins(){
+	for(int i = 0; i<7; i++){
+		pinMode(sd7_pins[i], OUTPUT);
+	}
+}
+
+void _2x7SegmentDisplay::Update(){
+      if(m_num != -1){
+        m_digit[0] = (m_num < 10)? 0 : m_num/10;
+        m_digit[1] = m_num%10;
+      }
+  
+      digitalWrite(13 - m_whichDigit, HIGH);
+      digitalWrite(13 - !m_whichDigit, LOW);
+
+      Display(m_digit[m_whichDigit]);
+
+      m_whichDigit = !m_whichDigit;
+    }
---
+++ arduino-7-segment-display-ctl.hpp
@@ -0,0 +1,70 @@
+/* arduino-7-segment-display-ctl.hpp
+ *
+ * Copyright 2021 Dominik Rzecki
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#pragma once
+
+#include <Arduino.h>
+
+class _7SegmentDisplay{
+  public:
+    _7SegmentDisplay(byte a, byte b, byte c, byte d, byte e, byte f, byte g, bool lowDriven = false);
+
+    _7SegmentDisplay(byte pins[7]);
+
+    void TurnOff();
+    void Display(int num);
+    
+  protected:
+  
+    byte sd7_pins[7];
+    bool sd7_lowDriven;
+    
+    void setPins();
+    
+    static const bool sd7_num[10][7]{
+      {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, LOW},  //0
+      {LOW, HIGH, HIGH, LOW, LOW, LOW, LOW},      //1
+      {HIGH, HIGH, LOW, HIGH, HIGH, LOW, HIGH},   //2
+      {HIGH, HIGH, HIGH, HIGH, LOW, LOW, HIGH},   //3
+      {LOW, HIGH, HIGH, LOW, LOW, HIGH, HIGH},    //4
+      {HIGH, LOW, HIGH, HIGH, LOW, HIGH, HIGH},   //5
+      {HIGH, LOW, HIGH, HIGH, HIGH, HIGH, HIGH},  //6
+      {HIGH, HIGH, HIGH, LOW, LOW, LOW, LOW},     //7
+      {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH}, //8
+      {HIGH, HIGH, HIGH, HIGH, LOW , HIGH, HIGH}   //9
+    };
+};
+
+class _2x7SegmentDisplay : private _7SegmentDisplay {
+  public:
+    _2x7SegmentDisplay(byte a, byte b, byte c, byte d, byte e, byte f, byte g, bool lowDriven = false) 
+      : SegmentDisplay7{a, b, c, d, e, f, g, lowDriven}
+      {}
+
+    inline void Set(int num){
+      m_num = num;
+    }
+
+    void Update();
+    
+  private:
+    bool m_whichDigit = 0;
+    int m_num;
+    short int m_digit[2];
+};
